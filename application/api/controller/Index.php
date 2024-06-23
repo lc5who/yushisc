8d234392cea9951c21031aef9749c77fa7eb244e
lc5who <31762907+lc5who@users.noreply.github.com>
2024-06-23 10:43:50 +0800

    dfadsf

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -26,4 +26,24 @@ class Index extends Api
     {
         $this->success('ok',Banner::all());
     }
+
+    public function rechargeMethod()
+    {
+        $this->success('获取成功',[
+            'bank'=>[
+                'bankName'=>'农业银行',
+                'name'=>'张三',
+                'card'=>'1231231231',
+                'status'=>1,
+            ],
+            'wx'=>[
+                'qrcode'=>'http://141.11.183.159/uploads/20240622/15db8a1d9209c9ca1416ac4d2308004f.png',
+                'status'=>1,
+            ],
+            'alipay'=>[
+                'qrcode'=>'http://141.11.183.159/uploads/20240622/15db8a1d9209c9ca1416ac4d2308004f.png',
+                'status'=>1,
+            ]
+        ]);
+    }
 }
