6cb7740039f48e39c4621bae195b6fff23afec0c
leo <lcclal@hotmail.com>
2024-06-30 06:11:46 +0800

    asdf

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -115,65 +115,17 @@ class Index extends Api
     }
     public function base64upload ()
     {
-        Config::set('default_return_type', 'json');
-        //必须设定cdnurl为空,否则cdnurl函数计算错误
-        Config::set('upload.cdnurl', '');
-        $chunkid = $this->request->post("chunkid");
-        $this->success();
-        if ($chunkid) {
-            if (!Config::get('upload.chunking')) {
-                $this->error(__('Chunk file disabled'));
-            }
-            $action = $this->request->post("action");
-            $chunkindex = $this->request->post("chunkindex/d");
-            $chunkcount = $this->request->post("chunkcount/d");
-            $filename = $this->request->post("filename");
-            $method = $this->request->method(true);
-            if ($action == 'merge') {
-                $attachment = null;
-                //合并分片文件
-                try {
-                    $upload = new Upload();
-                    $attachment = $upload->merge($chunkid, $chunkcount, $filename);
-                } catch (UploadException $e) {
-                    $this->error($e->getMessage());
-                }
-                $this->success(__('Uploaded successful'), ['url' => $attachment->url, 'fullurl' => cdnurl($attachment->url, true)]);
-            } elseif ($method == 'clean') {
-                //删除冗余的分片文件
-                try {
-                    $upload = new Upload();
-                    $upload->clean($chunkid);
-                } catch (UploadException $e) {
-                    $this->error($e->getMessage());
-                }
-                $this->success();
-            } else {
-                //上传分片文件
-                //默认普通上传文件
-                $file = $this->request->file('file');
-                try {
-                    $upload = new Upload($file);
-                    $upload->chunk($chunkid, $chunkindex, $chunkcount);
-                } catch (UploadException $e) {
-                    $this->error($e->getMessage());
-                }
-                $this->success();
-            }
-        } else {
-            $attachment = null;
-            //默认普通上传文件
-            $file = $this->request->file('file');
+            $base64_image = $this->request->file('base64');
+            $decoded_image = base64_decode($base64_image);
             try {
-                $upload = new Upload($file);
+                $upload = new Upload($decoded_image);
                 $attachment = $upload->upload();
             } catch (UploadException $e) {
                 $this->error($e->getMessage());
             } catch (\Exception $e) {
                 $this->error($e->getMessage());
             }
-
             $this->success(__('Uploaded successful'), ['url' => $attachment->url, 'fullurl' => cdnurl($attachment->url, true)]);
-        }
+
     }
 }
